a39c1b3ccb997a707f7a90150aa700bd38586d7d
sabrinahuotmilliard <sabrina.huotmilliard@hotmail.com>
2025-05-19 22:16:31 -0400

    ajout UI timer

---
+++ Assets/Scripts/GM_GestionTimer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+
+public class GM_GestionTimer : MonoBehaviour
+{
+    [SerializeField] float timer = 60f * 3f;
+    [SerializeField] GameObject timerUI;
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = 0;
+            // Call the function to end the game
+            //EndGame();
+        }
+        updateTimerUI();
+    }
+
+    private void updateTimerUI()
+    {
+        // Update the UI element with the remaining time
+        // Assuming timerUI has a Text component to display the time
+        int minutes = (int)(timer / 60);
+        int seconds = (int)(timer % 60);
+
+        string formattedTime = string.Format("{0}:{1:00}", minutes, seconds);
+        timerUI.GetComponent<TextMeshProUGUI>().text = formattedTime;
+    }
+}
